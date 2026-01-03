0969544731506f53b303f3cda8b13903e3f290a1
Noah Cohn <noahc42@gmail.com>
2026-01-03 07:01:06 -0800

    Update build-web.sh

--- build-web.sh
+++ build-web.sh
@@ -14,7 +14,7 @@ mkdir -p "$BUILD_DIR"
 
 # Run CMake
 echo "Running CMake configuration..."
-emcmake cmake -DCMAKE_CXX_FLAGS="-I/usr/local/include" -B "$BUILD_DIR" ../
+emcmake cmake -DCMAKE_CXX_FLAGS="-I/usr/local/include -I./web" -B "$BUILD_DIR" ../
 
 # Build
 echo "Building project..."
